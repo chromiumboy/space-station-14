a88bb1db5b4598147e60547df4fbc64e9a7b4cd7
chromiumboy <chromium.boy@gmail.com>
2024-04-18 22:31:01 -0500

    Fixed a stupid bug

--- Content.Server/Pinpointer/NavMapRegionSeedComponent.cs
+++ Content.Server/Pinpointer/NavMapRegionSeedComponent.cs
@@ -1,11 +1,9 @@
-using Robust.Shared.GameStates;
-
 namespace Content.Server.Pinpointer;
 
 /// <summary>
 /// Used to mark entities that are seeds for generating nav map regions on client UI
 /// </summary>
-[RegisterComponent, NetworkedComponent]
+[RegisterComponent]
 public sealed partial class NavMapRegionSeedComponent : Component
 {
 
