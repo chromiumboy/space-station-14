30745d637f8280f53d59aef5e5f60c08490ec2b9
chromiumboy <chromium.boy@gmail.com>
2025-01-30 00:53:33 -0600

    Beginning of turret controls system

--- Content.Shared/TurretControls/TurretControlsComponent.cs
+++ Content.Shared/TurretControls/TurretControlsComponent.cs
@@ -1,4 +1,7 @@
+using Content.Shared.Access;
+using Content.Shared.Turrets;
 using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization;
 
 namespace Content.Shared.TurretControls;
@@ -6,11 +9,36 @@ namespace Content.Shared.TurretControls;
 [RegisterComponent, NetworkedComponent]
 public sealed partial class TurretControlsComponent : Component
 {
+    public Dictionary<Entity<TurretTargetingComponent>, string> LinkedTurrets = new();
+}
 
+/// <summary>
+/// Sends a list from the client to the server informing the entity of changes to its turret control settings
+/// </summary>
+[Serializable, NetSerializable]
+public sealed class TurretControlSettingsChangedMessage : BoundUserInterfaceMessage
+{
+    public TurretControlsArmamentState? ArmamentState;
+    public bool? TargetCyborgs;
+    public bool? TargetBasicSilicons;
+    public bool? TargetAnimalsAndXenos;
+    public bool? TargetVisibleContraband;
+    public bool? TargetWantedCriminals;
+    public bool? TargetUnauthorizedCrew;
+    public HashSet<ProtoId<AccessLevelPrototype>>? AuthorizedAccessLevels;
+}
+
+[Serializable, NetSerializable]
+public enum TurretControlsArmamentState : byte
+{
+    Safe,
+    Stun,
+    Lethal,
 }
 
 [Serializable, NetSerializable]
 public enum TurretControlsUiKey : byte
 {
-    Key,
+    General,
+    StationAi,
 }
