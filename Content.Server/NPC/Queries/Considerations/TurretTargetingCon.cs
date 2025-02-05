fc998cd168434f4c7b57665470e6c705f952e812
chromiumboy <chromium.boy@gmail.com>
2025-02-05 00:54:40 -0600

    Updated turret targeting consideration

--- Content.Server/NPC/Queries/Considerations/TurretTargetingCon.cs
+++ Content.Server/NPC/Queries/Considerations/TurretTargetingCon.cs
@@ -1,4 +1,3 @@
-using Content.Shared.Access.Systems;
 using Content.Shared.Turrets;
 using Microsoft.Extensions.DependencyModel;
 
@@ -8,35 +7,24 @@ public sealed partial class TurretTargetingCon : UtilityConsideration
 {
     [Dependency] private readonly IEntityManager _entManager = default!;
 
-    private AccessReaderSystem _accessReader = default!;
+
+    private TurretTargetSettingsSystem _turretTargetSettings = default!;
 
     public override void Initialize(IEntitySystemManager sysManager)
     {
         base.Initialize(sysManager);
 
-        _accessReader = _entManager.System<AccessReaderSystem>();
+        _turretTargetSettings = _entManager.System<TurretTargetSettingsSystem>();
     }
 
     public override float GetScore(NPCBlackboard blackboard, EntityUid targetUid, UtilityConsideration consideration)
     {
         var owner = blackboard.GetValue<EntityUid>(NPCBlackboard.Owner);
 
-        if (!_entManager.TryGetComponent<TurretTargetSettingsComponent>(owner, out var targeting))
+        if (!_entManager.TryGetComponent<TurretTargetSettingsComponent>(owner, out var turretTargetSettings) ||
+            _turretTargetSettings.EntityIsTargetForTurret((owner, turretTargetSettings), targetUid))
             return 1f;
 
-        // Check for authorized access
-        var accessLevels = _accessReader.FindAccessTags(targetUid);
-
-        if (targeting.ExemptAccessLevels.Count > 0)
-        {
-            // If the entity has an exempt access level, they will be ignored as a potential target
-            foreach (var accessLevel in accessLevels)
-            {
-                if (targeting.ExemptAccessLevels.Contains(accessLevel))
-                    return 0f;
-            }
-        }
-
-        return 1f;
+        return 0f;
     }
 }
